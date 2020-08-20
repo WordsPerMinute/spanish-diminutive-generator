e6b27e8f3ab011e3fb81887aba2bb7a4a8db43e0
WordsPerMinute <git@coreyhodge.net>
2020-08-19 21:53:55 -0600

    got most cases working, working on code for the edge cases, words like pan and lluvia

--- src/components/WordGenerator.js
+++ src/components/WordGenerator.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+// import $ from 'jquery'
+// var googleTranslate = require('google-translate')(apiKey, options);
+const cheerio = require('cheerio');
+const request = require('request');
 
 const WordGenerator = (props) => {
   const [convertedWord, setConvertedWord] = useState('');
@@ -7,13 +10,64 @@ const WordGenerator = (props) => {
   const [userInput, setUserInput] = useState('');
   const [convertedWordDiv, setConvertedWordDiv] = useState(null);
 
+
+  const getWordGender = (word) => {
+    let gender = '';
+    console.log(word)
+    request({
+      method: 'GET',
+      url: `https://www.wordreference.com/definicion/${word}`,
+      
+    }, (err, res, body) => {
+      if (err) return console.error(err);
+      let $ = cheerio.load(body);
+      let wordGender = $('strong+ .POS2').text()[1];
+      console.log(wordGender)
+
+      if (wordGender === 'f') {
+        gender = 'female';
+      } else {
+        gender = 'male';
+      }
+    });
+    console.log(gender)
+    return gender;
+  }
+
   const convertWord = (word) => {
-    if (word[word.length - 1] === 'o') {
+
+    const countSyllables = (word) => {
+      word = word.toLowerCase();                                     //word.downcase!
+      if(word.length <= 3) { return 1; }                             //return 1 if word.length <= 3
+        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+        word = word.replace(/^y/, '');                                 //word.sub!(/^y/, '')
+        return word.match(/[aeiouy]{1,2}/g).length;                    //word.scan(/[aeiouy]{1,2}/).size
+    }
+
+
+
+    const syllableTotal = countSyllables(word)
+
+    const lastLetter = word[word.length - 1]
+
+    if (syllableTotal === 1) {
+      switch (lastLetter) {
+        case 's':
+          setConvertedWord(word.slice(0, [word.length - 1]) + 'cecitos')
+          break;
+        case 'n':
+          setConvertedWord(word + 'ecito');
+        default:
+          setConvertedWord(word + 'ecito');
+
+      }
+    } else if (word[word.length - 1] === 'o') {
       setConvertedWord(word.slice(0, [word.length - 1]) + 'ito')
+    } else if (word[word.length - 1] === 's') {
+      console.log('came here')
+
     } else if (word[word.length - 1] === 'a') {
       setConvertedWord(word.slice(0, [word.length - 1]) + 'ita')
-    }  else if (word[word.length - 1] === 'r') {
-      setConvertedWord(word + 'ito')
     } else if (
       word[word.length - 1] === ('a')
       || word[word.length - 1] === ('á')
@@ -25,8 +79,16 @@ const WordGenerator = (props) => {
       || word[word.length - 1] === ('ó')
       || word[word.length - 1] === ('u')
       || word[word.length - 1] === ('ú')
+      || word[word.length - 1] === ('n')
+      || word[word.length - 1] === ('r')
       ) {
       setConvertedWord(word + 'cito')
+    } else if (word[word.length - 1] === 'z') {
+      // use translation API for gender
+      // setConvertedWord(word.slice(0, [word.length - 1]) + 'cita')
+      // setConvertedWord(word.slice(0, [word.length - 1]) + 'cito')
+      let wordUP = getWordGender(word);
+      console.log(wordUP)
     } else {
       setConvertedWord(word + 'ito')
     }
@@ -48,19 +110,6 @@ const WordGenerator = (props) => {
   const onClick = (e) => {
     setShowConvertedWord(false);
     setUserInput(e.currentTarget.innerText)
-  };
-
-  const onKeyDown = (e) => {
-
-    if (e.keyCode === 13) {
-      return;
-    } else if (e.keyCode === 38) {
-      // up arrow
-      return;
-    } else if (e.keyCode === 40) {
-      // down arrow
-      return;
-    }
   };
 
   if (setConvertedWord && userInput) {
